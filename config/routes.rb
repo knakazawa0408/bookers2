3fdc0b663b46adad861acda5e84cb3094de50202
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-08-31 11:37:17 +0000

    [Add]ルーティング記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
   devise_for :users
   root to: "homes#top"
+
+  # get 'users/show'
+  # get 'users/edit'
+  resources :user, only: [:show, :edit, :update]
+  resources :books, only: [:new, :index, :show, :edit,]
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
