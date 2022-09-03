c475b469db2046d3da61e33af36abf2add58c3af
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-09-03 04:39:55 +0000

    [Fix]usersルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
 
-  # get 'users/show'
-  # get 'users/edit'
-  resources :user, only: [:show, :edit, :index, :update]
+  resources :users, only: [:show, :edit, :update]
   resources :books, only: [:new, :index, :show, :edit,]
-  # get 'books/new'
-  # get 'books/index'
-  # get 'books/show'
-  # get 'books/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
