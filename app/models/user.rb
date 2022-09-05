2d16aa50ba86e8a0b3123654b69527d119222e8d
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-09-05 16:53:22 +0000

    [Add]bookのindexページUser info作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
+  has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit]
   resources :books, only: [:new, :index, :show, :edit,]
 end
