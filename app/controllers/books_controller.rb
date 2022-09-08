cc59974f8a1857d269889a683540613611d39d0f
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-09-08 15:05:47 +0000

    [Add]bookのeditページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def new
   end
-  
+
   def create
     @book=Book.new(book_params)
     @book.user_id=current_user.id
@@ -20,12 +20,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
