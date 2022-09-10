12cf3603a19e80172ce2e55bf68eded162fe6c16
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-09-10 12:13:48 +0000

    [Add]ユーザー編集フラッシュメッセージ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,10 @@ class UsersController < ApplicationController
   
   def update
     @user=User.find(params[:id])
-    @user.update(user_params)
-    redirect_to "/books"
+    if @user.update(user_params)
+      flash[:success]="You have updated user successfully."
+     redirect_to "/books"
+    end
   end
   
   private
