f877529d2e820ce20d1ef91ce8048d12ab40d3ac
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-09-10 04:00:25 +0000

    [Add]userのupdate機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,16 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
   end
   
+  def update
+    @user=User.find(params[:id])
+    @user.update(user_params)
+    redirect_to "/books"
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
