2effc422241aa6a4140c883b91b6cccdb9c6c91d
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-09-11 00:17:52 +0000

    [Add]updateフラッシュメッセージ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,8 +27,10 @@ class BooksController < ApplicationController
 
   def update
     @book=Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:success]="You have updated book successfully."
+      redirect_to book_path(@book.id)
+    end
   end
 
   def destroy
