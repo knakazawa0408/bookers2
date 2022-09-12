e4ea557f0d1873827f7351620c511b4dc7dde3a5
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-09-12 09:48:43 +0000

    [Add]nameの一意性設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
 
-  validates :name, {presence: true, length: {minimum: 2, maximum: 20}}
+  validates :name, {presence: true, length: {minimum: 2, maximum: 20}, uniqueness: true}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
